14fb4ebca057517c36434f00829c6b7f8c99fde9
Aditya Prakash <115403112+Nickroger797@users.noreply.github.com>
2025-04-05 15:53:56 +0530

    Create script.py

---
+++ script.py
@@ -0,0 +1,14 @@
+start_text = "Send me any public Instagram reel link and I’ll download the video for you!"
+
+about_text = (
+    "🤖 **About This Bot:**\n\n"
+    "This bot allows you to download any public Instagram reel easily.\n"
+    "Built with ❤️ using Python, Pyrogram, and Instaloader."
+)
+
+help_text = (
+    "ℹ️ **Help Guide:**\n\n"
+    "1. Just send any *public Instagram reel link*.\n"
+    "2. Bot will fetch and send you the video.\n\n"
+    "**Note:** Private reels can't be downloaded."
+)
